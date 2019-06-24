2614bcba8289a5cf4b70025f4fbfef9bb4e84633
Ilga Yulian Putra D <ilga@homecare24.id>
2019-06-24 13:13:43 +0700

    Add movie detail screen

--- app/src/main/java/com/gyosanila/mymovie/features/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/gyosanila/mymovie/features/dashboard/DashboardActivity.kt
@@ -1,16 +1,21 @@
 package com.gyosanila.mymovie.features.dashboard
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.AdapterView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.gyosanila.mymovie.R
 import com.gyosanila.mymovie.features.adapter.MovieAdapter
+import com.gyosanila.mymovie.features.movieDetail.MovieDetailActivity
 import com.gyosanila.mymovie.features.network.Movie
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.content_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+
+    private val listMovie = ArrayList<Movie>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,26 +31,36 @@ class DashboardActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
         listViewMovie.adapter = MovieAdapter(this, setMovieList())
+        listViewMovie.onItemClickListener = AdapterView.OnItemClickListener {
+                _, _, position, _ ->  navigateToMovieDetail(listMovie[position])
+        }
     }
 
     @SuppressLint("Recycle")
     fun setMovieList(): ArrayList<Movie> {
-        val listMovie = ArrayList<Movie>()
         val dataTitle = resources.getStringArray(R.array.data_title)
         val dataDescription = resources.getStringArray(R.array.data_description)
         val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
         val dataPublish = resources.getStringArray(R.array.data_publish_at)
+        val dataDirector = resources.getStringArray(R.array.data_director)
         for (data in 0 until dataTitle.size) {
             val hero = Movie(
                 data,
                 dataTitle[data],
                 dataDescription[data],
                 dataPhoto.getResourceId(data, -1),
-                dataPublish[data]
+                dataPublish[data],
+                dataDirector[data]
             )
             listMovie.add(hero)
         }
         return listMovie
+    }
+
+    private fun navigateToMovieDetail(movie: Movie) {
+        val toMovieDetail = Intent(this, MovieDetailActivity::class.java)
+        toMovieDetail.putExtra("Movie", movie)
+        startActivity(toMovieDetail)
     }
 
 }
--- app/src/main/java/com/gyosanila/mymovie/features/network/Movie.kt
+++ app/src/main/java/com/gyosanila/mymovie/features/network/Movie.kt
@@ -1,5 +1,8 @@
 package com.gyosanila.mymovie.features.network
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /**
  * Created by ilgaputra15
  * on Sunday, 23/06/2019 22:41
@@ -8,8 +11,41 @@ package com.gyosanila.mymovie.features.network
 
 data class Movie(
     val id: Int,
-    val title: String,
-    val description: String,
+    val title: String?,
+    val description: String?,
     val photoResource: Int,
-    val publish_at: String
-)
+    val publish_at: String?,
+    val director: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeInt(photoResource)
+        parcel.writeString(publish_at)
+        parcel.writeString(director)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Movie> {
+        override fun createFromParcel(parcel: Parcel): Movie {
+            return Movie(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Movie?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
