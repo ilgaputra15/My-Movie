28253e73609cc5503d48864591dda991f6f25d4e
Ilga Yulian Putra D <ilga@homecare24.id>
2019-06-27 17:42:58 +0700

    Add bottom navigation on dashboard

--- app/src/main/java/com/gyosanila/mymovie/features/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/gyosanila/mymovie/features/dashboard/DashboardActivity.kt
@@ -1,26 +1,37 @@
 package com.gyosanila.mymovie.features.dashboard
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.AdapterView
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.gyosanila.mymovie.R
-import com.gyosanila.mymovie.features.adapter.MovieAdapter
-import com.gyosanila.mymovie.features.movieDetail.MovieDetailActivity
-import com.gyosanila.mymovie.features.network.Movie
+import com.gyosanila.mymovie.core.extension.changeFragment
+import com.gyosanila.mymovie.features.fragmentMovie.FragmentMovie
+import com.gyosanila.mymovie.features.fragmentTvShow.FragmentTvShow
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.content_dashboard.*
 
-class DashboardActivity : AppCompatActivity() {
-
-    private val listMovie = ArrayList<Movie>()
+class DashboardActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         setupUI()
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when(menuItem.itemId){
+            R.id.navigation_movies -> {
+                fragmentMovies()
+                return true
+            }
+            R.id.navigation_tv_show -> {
+                fragmentTvShow()
+                return true
+            }
+        }
+        return false
     }
 
     private fun setupUI() {
@@ -30,37 +41,20 @@ class DashboardActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        listViewMovie.adapter = MovieAdapter(this, setMovieList())
-        listViewMovie.onItemClickListener = AdapterView.OnItemClickListener {
-                _, _, position, _ ->  navigateToMovieDetail(listMovie[position])
-        }
+        fragmentMovies()
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
     }
 
-    @SuppressLint("Recycle")
-    fun setMovieList(): ArrayList<Movie> {
-        val dataTitle = resources.getStringArray(R.array.data_title)
-        val dataDescription = resources.getStringArray(R.array.data_description)
-        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
-        val dataPublish = resources.getStringArray(R.array.data_publish_at)
-        val dataDirector = resources.getStringArray(R.array.data_director)
-        for (data in 0 until dataTitle.size) {
-            val hero = Movie(
-                data,
-                dataTitle[data],
-                dataDescription[data],
-                dataPhoto.getResourceId(data, -1),
-                dataPublish[data],
-                dataDirector[data]
-            )
-            listMovie.add(hero)
-        }
-        return listMovie
+    private fun fragmentMovies() {
+        toolbar_title.text = getString(R.string.text_title_movie)
+        val fragmentMovie = FragmentMovie()
+        this.changeFragment(fragmentMovie)
     }
 
-    private fun navigateToMovieDetail(movie: Movie) {
-        val toMovieDetail = Intent(this, MovieDetailActivity::class.java)
-        toMovieDetail.putExtra("Movie", movie)
-        startActivity(toMovieDetail)
+    private fun fragmentTvShow() {
+        toolbar_title.text = getString(R.string.text_title_tv_show)
+        val fragmentTvShow = FragmentTvShow()
+        this.changeFragment(fragmentTvShow)
     }
-
 }
